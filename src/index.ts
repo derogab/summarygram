4707d2c6304546865bcbb73147dffafc8913890f
Gabriele De Rosa <derosagabriele@outlook.it>
2025-10-18 23:28:45 +0200

    feat: send summaries for all chats at 23:59

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 // Dependencies.
 import { Bot } from "grammy";
 import * as dotenv from 'dotenv';
+import cron from 'node-cron';
 
 import Storage from './utils/data';
-import { onMessageReceived } from './controller/core';
+import { onCronJob, onMessageReceived } from './controller/core';
 
 // Configs.
 dotenv.config();
@@ -16,6 +17,9 @@ const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN ?? '');
 
 // Add message event listener(s).
 bot.on('message', async (ctx) => onMessageReceived(storage, ctx));
+
+// Add cron job listener(s).
+cron.schedule('59 23 * * *', () => onCronJob(storage, bot));
 
 // Start bot.
 bot.start({ allowed_updates: ["message"] })
